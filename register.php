1e5bb045a65abffc5d4ccca8a16eb91e7bcb3ed7
eoogmea <45141502+eoogmea@users.noreply.github.com>
2018-11-18 11:12:22 +0000

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://{arcobot1}.herokuapp.com/execute.php';
+$BOT_TOKEN = '707890731:AAGUYEz1dLYghibXihZrNjrg1Dw7v2nj1pI';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
